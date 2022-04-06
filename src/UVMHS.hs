b5b45212ba294c39642a7f5c58c910bbc5225691
David Darais <david.darais@gmail.com>
2022-04-06 14:09:52 -0600

    forgot to add Graph.hs to UVMHS.hs exports

--- src/UVMHS.hs
+++ src/UVMHS.hs
@@ -4,6 +4,7 @@ module UVMHS
   , module UVMHS.Lang.ULCD
   , module UVMHS.Lib.Annotated
   , module UVMHS.Lib.Errors
+  , module UVMHS.Lib.Graph
   , module UVMHS.Lib.Logging
   , module UVMHS.Lib.MMSP
   , module UVMHS.Lib.Options
@@ -22,6 +23,7 @@ import UVMHS.CoreTests
 import UVMHS.Lang.ULCD
 import UVMHS.Lib.Annotated
 import UVMHS.Lib.Errors
+import UVMHS.Lib.Graph
 import UVMHS.Lib.Logging
 import UVMHS.Lib.MMSP
 import UVMHS.Lib.Options
